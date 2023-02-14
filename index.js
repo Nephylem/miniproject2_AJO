223916e4139fff56eb4bc6d47b6f03425fda1676
Paul <sk8butt420@gmail.com>
2023-02-14 02:47:57 -0600

    added view functionality (unfinished)

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
+// for product view modal
+
+let viewProduct = document.querySelectorAll('.fa-eye');
+let productModal = document.querySelector('.modal-overlay');
+let productCloseBtn = document.querySelector('.fa-times')
+viewProduct.forEach((value, index)=>{
+    value.addEventListener('click', ()=>{
+        productModal.classList.add('active')
+    })
+})
+
+productCloseBtn.addEventListener('click', ()=>{
+    productModal.classList.remove('active')
+})
+
+var swiper = new Swiper(".product-swiper", {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".product-swiper2", {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: swiper,
+    },
+});
+
+
+// for navbar
 let menubar = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
-
-
-
+ 
 menubar.addEventListener('click', ()=>{
     menubar.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 })
 
-
+// for home carousel
 var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
@@ -23,6 +55,7 @@ var swiper = new Swiper(".home-slider", {
     loop:true,
 });
 
+// for reviews carousel
 var swiper = new Swiper(".reviews-swiper", {
     spaceBetween: 20,
     centeredSlides: true,
@@ -50,6 +83,8 @@ var swiper = new Swiper(".reviews-swiper", {
         },
     }
 });
+
+// for second reviews carousel
 var swiper = new Swiper(".reviews-swiper-two", {
     spaceBetween: 20,
     centeredSlides: true,

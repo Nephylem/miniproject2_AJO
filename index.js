bf31f807336619ee09d8d9acb7f239c2d47d5d37
Paul <sk8butt420@gmail.com>
2023-02-15 00:56:52 -0600

    modified product view modal functionality

--- index.js
+++ index.js
@@ -1,34 +1,71 @@
 // for product view modal
 
-let productObj = [
-belly = {
-        img : ["index_embed/products/belly/AJO_main.jpg", "index_embed/products/belly/AJO_open.jpg","index_embed/products/belly/AJO_close.jpg","index_embed/products/belly/AJO_chopped.jpg",
-        "index_embed/products/belly/AJO_chopped_open.jpg"]
-    },
-]
-
-let swiperModalWrapper = document.querySelector('.product-wrapper');
-let swiperModalWrapper2 = document.querySelectorAll('.product-wrapper')[1];
 
 
+let productObj = {
+    belly_img : ["index_embed/products/belly/AJO_main.jpg", "index_embed/products/belly/AJO_open.jpg","index_embed/products/belly/AJO_close.jpg","index_embed/products/belly/AJO_chopped.jpg",
+    "index_embed/products/belly/AJO_chopped_open.jpg"],
+    belly_text : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda quam cum iste nobis, id ut, nesciunt nulla, in quod debitis exercitationem dignissimos asperiores perferendis? Quaerat alias voluptatum nostrum voluptas dolorum?",
+    chili_garlic_img : ["index_embed/products/chili_garlic/chili_garlic.jpg","index_embed/products/chili_garlic/chili_garlic2.jpg","index_embed/products/chili_garlic/chili_garlic3.jpg","index_embed/products/chili_garlic/chili_garlic4.jpg"],
+    chili_garlic_text : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda quam cum iste nobis, id ut, nesciunt nulla, in quod debitis exercitationem dignissimos asperiores perferendis? Quaerat alias voluptatum nostrum voluptas dolorum?",
+    cochinillio_img : ["index_embed/products/cochinillo/cochinillio.jpg", "index_embed/products/cochinillo/cochinillio2.jpg", "index_embed/products/cochinillo/cochinillio3.jpg", "index_embed/products/cochinillo/cochinillio4.jpg"],
+    cochinillio_text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda quam cum iste nobis, id ut, nesciunt nulla, in quod debitis exercitationem dignissimos asperiores perferendis? Quaerat alias voluptatum nostrum voluptas dolorum?",
+    sisig_img : ["index_embed/products/sisig/AJO_sisig.jpg", "index_embed/products/sisig/AJO_sisig2.jpg", "index_embed/products/sisig/AJO_sisig3.jpg", "index_embed/products/sisig/AJO_sisig4.jpg"],
+    sisig_text : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda quam cum iste nobis, id ut, nesciunt nulla, in quod debitis exercitationem dignissimos asperiores perferendis? Quaerat alias voluptatum nostrum voluptas dolorum?", 
+    vinegar_img : ["index_embed/products/vinegar/vinegar1.jpg", "index_embed/products/vinegar/vinegar2.jpg"],
+    vinegar_text : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda quam cum iste nobis, id ut, nesciunt nulla, in quod debitis exercitationem dignissimos asperiores perferendis? Quaerat alias voluptatum nostrum voluptas dolorum?",
+}
 
-for (product in productObj){    
-    let img = productObj[product].img
 
-    img.forEach((value, index)=>{
-        let divElementOne = elementGenerator('div', 'class', 'swiper-slide product-slide'); 
-        let imgElementOne = elementGenerator('img', 'src', value);
-        let divElementTwo = elementGenerator('div', 'class', 'swiper-slide product-slide'); 
-        let imgElementTwo = elementGenerator('img', 'src', value);
-        
-        divElementOne.appendChild(imgElementOne)
-        divElementTwo.appendChild(imgElementTwo)
+modalInfoGallery(productObj.belly_img, productObj.belly_text, 0)
+modalInfoGallery(productObj.chili_garlic_img,productObj.chili_garlic_text, 1)
+modalInfoGallery(productObj.cochinillio_img, productObj.cochinillio_text, 2)
+modalInfoGallery(productObj.sisig_img, productObj.sisig_text, 3)
+modalInfoGallery(productObj.vinegar_img, productObj.vinegar_text, 4)
 
-        swiperModalWrapper.appendChild(divElementOne)
-        swiperModalWrapper2.appendChild(divElementTwo)
-        
+
+
+// a function to add content in the modal product view
+function modalInfoGallery(objImg, objText, productIndex){
+    let productInfoText = document.querySelector('.product-info p')
+    let swiperModalWrapper = document.querySelectorAll('.product-wrapper')[0];
+    let swiperModalWrapper2 = document.querySelectorAll('.product-wrapper')[1];
+    let viewProduct = document.querySelectorAll('.fa-eye');
+    let productModal = document.querySelector('.modal-overlay');
+    let productCloseBtn = document.querySelector('.fa-times')
+    viewProduct[productIndex].addEventListener('click', ()=>{
+        productModal.classList.add('active')
+        objImg.forEach((value,index)=>{
+            let divElementOne = elementGenerator('div', 'class', 'swiper-slide product-slide');
+            let imgElementOne = elementGenerator('img', 'src', value);
+            divElementOne.appendChild(imgElementOne);
+    
+    
+            let divElementTwo = elementGenerator('div', 'class', 'swiper-slide product-slide');
+            let imgElementTwo = elementGenerator('img', 'src', value);
+            divElementTwo.appendChild(imgElementTwo);
+    
+            swiperModalWrapper.appendChild(divElementOne);
+            swiperModalWrapper2.appendChild(divElementTwo);
+
+            productInfoText.innerHTML = objText
+        })
+    
+        productCloseBtn.addEventListener('click', ()=>{
+            productModal.classList.remove('active');
+            while(swiperModalWrapper.firstChild){
+                swiperModalWrapper.removeChild(swiperModalWrapper.firstChild)
+            }
+            while(swiperModalWrapper2.firstChild){
+                swiperModalWrapper2.removeChild(swiperModalWrapper2.firstChild)
+            }
+
+            productInfoText = ""
+        })
     })
 }
+
+
 
 
 
@@ -44,19 +81,6 @@ function elementGenerator(element, attribute, value){
 }
 
 
-
-let viewProduct = document.querySelectorAll('.fa-eye');
-let productModal = document.querySelector('.modal-overlay');
-let productCloseBtn = document.querySelector('.fa-times')
-viewProduct.forEach((value, index)=>{
-    value.addEventListener('click', ()=>{
-        productModal.classList.add('active')
-    })
-})
-
-productCloseBtn.addEventListener('click', ()=>{
-    productModal.classList.remove('active')
-})
 
 var swiper = new Swiper(".product-swiper", {
       spaceBetween: 10,

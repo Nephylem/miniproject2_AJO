f15481aff482f32e8e5ca22c896da68c9bcd318d
Paul <sk8butt420@gmail.com>
2023-02-25 17:36:37 +0800

    cart total and qty fixed

--- index.js
+++ index.js
@@ -218,8 +218,6 @@ var swiper = new Swiper(".reviews-swiper-two", {
   },
 });
 
-
-
 let productPrice = [
   {
     name: "Lechon Belly",
@@ -536,8 +534,6 @@ sisigQtyVarBtn(
   sisig_flavor_variation
 );
 
-
-
 // for messenger widget
 // <!-- Your SDK code -->
 
@@ -584,120 +580,121 @@ window.onscroll = () => {
   });
 };
 
-
-
-
-
-
 // cart functionality
-const addToCart = document.getElementsByClassName('add-to-cart');
-const productRow = document.getElementsByClassName('product-row');
+const addToCart = document.getElementsByClassName("add-to-cart");
+const productRow = document.getElementsByClassName("product-row");
 
 for (var i = 0; i < addToCart.length; i++) {
   button = addToCart[i];
-  button.addEventListener('click', addToCartClicked)
+  button.addEventListener("click", addToCartClicked);
 }
 
-function addToCartClicked (event) {
+function addToCartClicked(event) {
   button = event.target;
   var cartItem = button.parentElement;
-  var price = cartItem.getElementsByClassName('product-price')[0].innerText;
-  
-  var imageSrc = cartItem.getElementsByClassName('product-image')[0].src;
-  addItemToCart (price, imageSrc);
-  updateCartPrice()
+  var price = cartItem.getElementsByClassName("product-price")[0].innerText;
+  var imageSrc = cartItem.getElementsByClassName("product-image")[0].src;
+  var bellyQtyVarBtn =
+    cartItem.getElementsByClassName("variation-qty")[0].innerText;
+
+  addItemToCart(price, imageSrc, bellyQtyVarBtn);
+  updateCartPrice();
 }
 
-function addItemToCart (price, imageSrc) {
-  var productRow = document.createElement('div');
-  productRow.classList.add('product-row');
-  var productRows = document.getElementsByClassName('product-rows')[0];
-  var cartImage = document.getElementsByClassName('cart-image');
-  
-  for (var i = 0; i < cartImage.length; i++){
-    if (cartImage[i].src == imageSrc){
-      alert ('This item has already been added to the cart')
+function addItemToCart(price, imageSrc, bellyQtyVarBtn) {
+  var productRow = document.createElement("div");
+  productRow.classList.add("product-row");
+  var productRows = document.getElementsByClassName("product-rows")[0];
+  var cartImage = document.getElementsByClassName("cart-image");
+
+  for (var i = 0; i < cartImage.length; i++) {
+    if (cartImage[i].src == imageSrc) {
+      alert("This item has already been added to the cart");
       return;
     }
   }
-  
+
   var cartRowItems = `
   <div class="product-row">
         <img class="cart-image" src="${imageSrc}" alt="">
         <span class ="cart-price">${price}</span>
-        <input class="product-quantity" type="number" value="1">
+        <span class ="product-quantity">${bellyQtyVarBtn}</span>
         <button class="remove-btn">Remove</button>
         </div>
         
-      `
+      `;
   productRow.innerHTML = cartRowItems;
   productRows.append(productRow);
-  productRow.getElementsByClassName('remove-btn')[0].addEventListener('click', removeItem)
-  productRow.getElementsByClassName('product-quantity')[0].addEventListener('change', changeQuantity)
-  updateCartPrice()
+  productRow
+    .getElementsByClassName("remove-btn")[0]
+    .addEventListener("click", removeItem);
+  productRow
+    .getElementsByClassName("product-quantity")[0]
+    .addEventListener("change", changeQuantity);
+  updateCartPrice();
 }
 // Remove products from cart
-const removeBtn = document.getElementsByClassName('remove-btn');
+const removeBtn = document.getElementsByClassName("remove-btn");
 for (var i = 0; i < removeBtn.length; i++) {
-  button = removeBtn[i]
-  button.addEventListener('click', removeItem)
+  button = removeBtn[i];
+  button.addEventListener("click", removeItem);
 }
 
-function removeItem (event) {
-  btnClicked = event.target
-  btnClicked.parentElement.parentElement.remove()
-  updateCartPrice()
+function removeItem(event) {
+  btnClicked = event.target;
+  btnClicked.parentElement.parentElement.remove();
+  updateCartPrice();
 }
 
 // update quantity input
-var quantityInput = document.getElementsByClassName('product-quantity')[0];
+var quantityInput = document.getElementsByClassName("product-quantity")[0];
 
-for (var i = 0; i < quantityInput; i++){
-  input = quantityInput[i]
-  input.addEventListener('change', changeQuantity)
+for (var i = 0; i < quantityInput; i++) {
+  input = quantityInput[i];
+  input.addEventListener("change", changeQuantity);
 }
 
 function changeQuantity(event) {
-  var input = event.target
-  if (isNaN(input.value) || input.value <= 0){
-    input.value = 1
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
   }
-  updateCartPrice()
+  updateCartPrice();
 }
 
 // update total price
 function updateCartPrice() {
-  var total = 0
+  var total = 0;
   for (var i = 0; i < productRow.length; i += 2) {
-    cartRow = productRow[i]
-  var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-  var quantityElement = cartRow.getElementsByClassName('product-quantity')[0]
-  var price = parseFloat(priceElement.innerText.replace('₱', ''))
-  var quantity = quantityElement.value
-  total = total + (price * quantity )
-    
-  }
-  document.getElementsByClassName('total-price')[0].innerText =  '₱' + total
+    cartRow = productRow[i];
+    var priceElement = cartRow.getElementsByClassName("cart-price")[0];
+    var quantityElement = cartRow.getElementsByClassName("product-quantity")[0];
+    var price = priceElement.innerText.replace("₱", "").replace(",", "");
 
-document.getElementsByClassName('cart-quantity')[0].textContent = i /= 2
+    var quantity = quantityElement.value;
+    total = total + Number(price);
+  }
+
+  document.getElementsByClassName("total-price")[0].innerText =
+    "₱" + total.toLocaleString("en-US") + ".00";
+  document.getElementsByClassName("cart-quantity")[0].textContent = i /= 2;
 }
 
 // purchase items
-const purchaseBtn = document.querySelector('.purchase-btn');
+const purchaseBtn = document.querySelector(".purchase-btn");
 
-const closeCartModal = document.querySelector('.cart-modal');
+const closeCartModal = document.querySelector(".cart-modal");
 
-purchaseBtn.addEventListener('click', purchaseBtnClicked)
+purchaseBtn.addEventListener("click", purchaseBtnClicked);
 
-function purchaseBtnClicked () {
-  alert ('Thank you for your purchase');
-  cartModalOverlay.style.transform= 'translateX(-100%)'
- var cartItems = document.getElementsByClassName('product-rows')[0]
- while (cartItems.hasChildNodes()) {
-   cartItems.removeChild(cartItems.firstChild)
-   
- }
-  updateCartPrice()
+function purchaseBtnClicked() {
+  alert("Thank you for your purchase");
+  cartModalOverlay.style.transform = "translateX(-100%)";
+  var cartItems = document.getElementsByClassName("product-rows")[0];
+  while (cartItems.hasChildNodes()) {
+    cartItems.removeChild(cartItems.firstChild);
+  }
+  updateCartPrice();
 }
 // end of purchase items
 
@@ -706,57 +703,53 @@ function purchaseBtnClicked () {
 // for products add to cart functionality
 
 // open cart modal
-const cart = document.querySelector('#cart');
-const cartModalOverlay = document.querySelector('.cart-modal-overlay');
+const cart = document.querySelector("#cart");
+const cartModalOverlay = document.querySelector(".cart-modal-overlay");
 
-cart.addEventListener('click', () => {
-  if (cartModalOverlay.style.transform === 'translateX(-200%)'){
-    cartModalOverlay.style.transform = 'translateX(0)';
+cart.addEventListener("click", () => {
+  if (cartModalOverlay.style.transform === "translateX(-200%)") {
+    cartModalOverlay.style.transform = "translateX(0)";
   } else {
-    cartModalOverlay.style.transform = 'translateX(-200%)';
+    cartModalOverlay.style.transform = "translateX(-200%)";
   }
-})
+});
 // end of open cart modal
 
 // close cart modal
-const closeBtn = document.querySelector ('#close-btn');
+const closeBtn = document.querySelector("#close-btn");
 
-closeBtn.addEventListener('click', () => {
-  cartModalOverlay.style.transform = 'translateX(-200%)';
+closeBtn.addEventListener("click", () => {
+  cartModalOverlay.style.transform = "translateX(-200%)";
 });
 
-cartModalOverlay.addEventListener('click', (e) => {
-  if (e.target.classList.contains('cart-modal-overlay')){
-    cartModalOverlay.style.transform = 'translateX(-200%)'
+cartModalOverlay.addEventListener("click", (e) => {
+  if (e.target.classList.contains("cart-modal-overlay")) {
+    cartModalOverlay.style.transform = "translateX(-200%)";
   }
-})
-
-
-
-
-// for log in modal 
-
-
-let loginBtn = document.querySelector('.icons .fa-user');
-let loginOverlay = document.querySelector('.login-modal-overlay');
-let loginCloseBtn =  document.querySelector('.login-modal-overlay .fa-times');
-
-loginBtn.addEventListener('click', ()=>{
-  loginOverlay.classList.add('active')
-})
-
-loginCloseBtn.addEventListener('click', ()=>{
-  loginOverlay.classList.remove('active')
-})
-
-let signUpButton = document.querySelector('#signUp');
-let signInButton = document.querySelector('#signIn');
-let container = document.querySelector('.modal-for-login');
-
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
 });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+// for log in modal
+
+let loginBtn = document.querySelector(".icons .fa-user");
+let loginOverlay = document.querySelector(".login-modal-overlay");
+let loginCloseBtn = document.querySelector(".login-modal-overlay .fa-times");
+
+loginBtn.addEventListener("click", () => {
+  loginOverlay.classList.add("active");
+});
+
+loginCloseBtn.addEventListener("click", () => {
+  loginOverlay.classList.remove("active");
+});
+
+let signUpButton = document.querySelector("#signUp");
+let signInButton = document.querySelector("#signIn");
+let container = document.querySelector(".modal-for-login");
+
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
 });
